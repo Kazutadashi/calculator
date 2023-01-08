f4fb8100290c2d2795c6815cac3d5f4670833fac
Kazutadashi <owenvl@yahoo.com>
2023-01-08 13:55:05 -0700

    Add files and add basic functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(x,y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x-y;
+}
+
+function divide(x,y){
+    if (y === 0){
+        return "Cannot divide by zero.";
+    }
+    else {
+        return x/y
+    }
+}
+
+function multiply(x,y){
+    return x*y;
+}
